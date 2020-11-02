f26b20c8e7e8007af88ebe53822b0592bbed0558
Erwan Wattelier <erwan.wattelier@isen.yncrea.fr>
2020-11-02 15:36:03 +0100

    Modif referee
    erreur provenant de variable inexistante actuellement

--- Basket/src/main/java/basket/referee.java
+++ Basket/src/main/java/basket/referee.java
@@ -15,14 +15,28 @@ public class referee {
     public void arbitreDit(){
         System.out.print("L'arbitre dit au micro: ");
     }
-    public void let() {
+   /* public void let() {
         arbitreDit();
         System.out.println("Il y a let. Veuillez recommencer à servir");
-    }
+    }*/
 
     public void faute() {
         arbitreDit();
         System.out.println("Faute");
+        if (team.fault>5) {
+            System.out.println("2 lancers");
+        }
+    }
+    
+    public void faute_sur_tir() {
+        arbitreDit();
+        System.out.println("Faute");
+        if (panier_marqué == 1){
+            System.out.println("Panier accordé, 1 lancer");
+        }
+        else{
+            System.out.println("2 lancers");
+        }
     }
     
  /*    public void annonceScoreEchange(Joueur joueur1, Joueur joueur2, String score1, String score2){
